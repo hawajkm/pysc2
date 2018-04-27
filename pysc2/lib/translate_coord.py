53aa2c2a51129764116d20bfef407f4160688fe5
Khalid Al-Hawaj <hawajkm@gmail.com>
2018-04-27 18:34:43 -0400

    [pysc2/lib/translate_coord] Fix screen translation
    Take into account position of screen in the world

--- pysc2/lib/translate_coord.py
+++ pysc2/lib/translate_coord.py
@@ -31,8 +31,9 @@ class translate_coord(object):
     self._minimap_size = None
     self._screen_size  = None
     self._camera_width = None
+    self._camera_pos   = None
 
-  def update(self, map_size, minimap_size, screen_size, camera_width):
+  def update(self, map_size, minimap_size, screen_size, camera_width, camera_pos):
 
     # Require updates
     needs_update = False
@@ -54,6 +55,10 @@ class translate_coord(object):
       self._camera_width = camera_width
       needs_update = True
 
+    if self._camera_pos != camera_pos:
+      self._camera_pos = camera_pos
+      needs_update = True
+
     # If an update is needed
     if needs_update:
       self.update_transformations()
@@ -72,16 +77,30 @@ class translate_coord(object):
         transform.Floor())
 
     # Flip and zoom to the camera area. Update the offset as the camera moves.
-    self._world_to_screen = transform.Linear(point.Point(1, -1),
-                                             point.Point(0, self._map_size.y))
+    # Get camera corner
+    cam_x   = -1 * (self._camera_pos['x'] - (self._camera_width / 2))
+    cam_y   =  1 * (self._camera_pos['y'] + (self._camera_width / 2))
+    cam_pos = point.Point(x = cam_x, y = cam_y)
+
+    self._reorient_world  = transform.Linear(point.Point(1, -1), offset=cam_pos)
+    self._world_to_screen = transform.Linear(point.Point(1, 1),
+                                             point.Point(0, 0))
+    #self._world_to_screen = transform.Linear(point.Point(1, -1),
+    #                                         point.Point(0, self._map_size.y))
     self._screen_to_fl_screen = transform.Linear(
         self._screen_size / self._camera_width)
     self._world_to_fl_screen = transform.Chain(
+        self._reorient_world,
         self._world_to_screen,
         self._screen_to_fl_screen,
         transform.Floor())
 
-  def world_to_minimap(self, x, y = None):
+  #-----------------------------------------------------------------------
+  # Translation Meta-Function
+  #-----------------------------------------------------------------------
+
+  @staticmethod
+  def __translate__(tf, tf_dir, x, y = None):
 
     # Either a point or a distance
     pt = None
@@ -100,87 +119,65 @@ class translate_coord(object):
     elif (type(x) is float) and (y == None):
       dist = x
 
-    if pt:
-      return self._world_to_fl_minimap.fwd_pt  (pt  )
-    elif dist:
-      return self._world_to_fl_minimap.fwd_dist(dist)
+    func_pt   = getattr(tf, '{}_pt'  .format(tf_dir))
+    func_dist = getattr(tf, '{}_dist'.format(tf_dir))
+
+    if   pt   and func_pt:
+      return func_pt  (pt  )
+    elif dist and func_dist:
+      return func_dist(dist)
     else:
       raise ValueError
+
+  #-----------------------------------------------------------------------
+  # World to Minimap
+  #-----------------------------------------------------------------------
+
+  def world_to_minimap(self, x, y = None):
+
+    cls  = self.__class__
+    func = cls.__translate__
+    tf   = self._world_to_fl_minimap
+    tf_d = 'fwd'
+
+    return func(tf, tf_d, x, y)
+
+  #-----------------------------------------------------------------------
+  # Minimap to World
+  #-----------------------------------------------------------------------
 
   def minimap_to_world(self, x, y = None):
 
-    # Either a point or a distance
-    pt = None
-    dist = None
+    cls  = self.__class__
+    func = cls.__translate__
+    tf   = self._world_to_fl_minimap
+    tf_d = 'back'
 
-    # Convert any integers to floats
-    if type(x) is int:
-      x = float(x)
-    if type(y) is int:
-      y = float(y)
+    return func(tf, tf_d, x, y)
 
-    if type(x) is point.Point:
-      pt = x
-    elif (type(x) is float) and (type(y) is float):
-      pt = point.Point(x, y)
-    elif (type(x) is float) and (y == None):
-      dist = x
-
-    if pt:
-      return self._world_to_fl_minimap.back_pt  (pt  )
-    elif dist:
-      return self._world_to_fl_minimap.back_dist(dist)
-    else:
-      raise ValueError
+  #-----------------------------------------------------------------------
+  # World to Screen
+  #-----------------------------------------------------------------------
 
   def world_to_screen(self, x, y = None):
 
-    # Either a point or a distance
-    pt = None
-    dist = None
+    cls  = self.__class__
+    func = cls.__translate__
+    tf   = self._world_to_fl_screen
+    tf_d = 'fwd'
 
-    # Convert any integers to floats
-    if type(x) is int:
-      x = float(x)
-    if type(y) is int:
-      y = float(y)
+    return func(tf, tf_d, x, y)
 
-    if type(x) is point.Point:
-      pt = x
-    elif (type(x) is float) and (type(y) is float):
-      pt = point.Point(x, y)
-    elif (type(x) is float) and (y == None):
-      dist = x
-
-    if pt:
-      return self._world_to_fl_screen.fwd_pt  (pt  )
-    elif dist:
-      return self._world_to_fl_screen.fwd_dist(dist)
-    else:
-      raise ValueError
+  #-----------------------------------------------------------------------
+  # Screen to World
+  #-----------------------------------------------------------------------
 
   def screen_to_world(self, x, y = None):
 
-    # Either a point or a distance
-    pt = None
-    dist = None
+    cls  = self.__class__
+    func = cls.__translate__
+    tf   = self._world_to_fl_screen
+    tf_d = 'back'
 
-    # Convert any integers to floats
-    if type(x) is int:
-      x = float(x)
-    if type(y) is int:
-      y = float(y)
+    return func(tf, tf_d, x, y)
 
-    if type(x) is point.Point:
-      pt = x
-    elif (type(x) is float) and (type(y) is float):
-      pt = point.Point(x, y)
-    elif (type(x) is float) and (y == None):
-      dist = x
-
-    if pt:
-      return self._world_to_fl_screen.back_pt  (pt  )
-    elif dist:
-      return self._world_to_fl_screen.back_dist(dist)
-    else:
-      raise ValueError
